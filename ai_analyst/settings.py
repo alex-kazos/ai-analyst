eb23a801265613c18700fb3c85d02121a675053f
Alexkazos <kazosalexandros@gmail.com>
2025-06-09 09:47:50 +0300

    feat: initialize Django project settings with authentication and file upload configurations

--- ai_analyst/settings.py
+++ ai_analyst/settings.py
@@ -16,7 +16,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1','ai-analyst-qh9m.onrender.com']
 
 # Application definition
 INSTALLED_APPS = [
