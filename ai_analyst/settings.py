308b1f55f5713857e04e81242734e9ff671a10d3
Alexkazos <kazosalexandros@gmail.com>
2025-06-22 20:14:25 +0300

    feat: initialize Django settings with authentication, CORS, and file upload configurations

--- ai_analyst/settings.py
+++ ai_analyst/settings.py
@@ -16,7 +16,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1','ai-analyst-qh9m.onrender.com']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 # Application definition
 INSTALLED_APPS = [
